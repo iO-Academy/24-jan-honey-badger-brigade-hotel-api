10b36f310ca276f8fbbc3aead732d7daf4905d0f
Szabó Gergő <szabogergo20011008@gmail.com>
2024-03-25 16:24:03 +0100

    booking controller update

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -1,10 +1,51 @@
 <?php
 
+
+// app/Http/Controllers/BookingController.php
+
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
+use App\Models\Room;
 use Illuminate\Http\Request;
 
-class RoomController extends Controller
+class BookingController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $request->validate([
+            'room_id' => 'required|exists:rooms,id',
+            'customer' => 'required|string',
+            'guests' => 'required|integer',
+            'start' => 'required|date|before_or_equal:end',
+            'end' => 'required|date|after_or_equal:start',
+        ]);
+
+        $isRoomAvailable = Booking::where('room_id', $request->room_id)
+            ->where(function ($query) use ($request) {
+                $query->whereBetween('start', [$request->start, $request->end])
+                    ->orWhereBetween('end', [$request->start, $request->end]);
+            })
+            ->doesntExist();
+
+        if (!$isRoomAvailable) {
+            return response()->json(['message' => 'The room is already booked for the given dates.'], 400);
+        }
+
+        $room = Room::findOrFail($request->room_id);
+        if ($request->guests > $room->max_capacity) {
+            return response()->json(['message' => 'The number of guests exceeds the room\'s capacity.'], 400);
+        }
+
+        $booking = Booking::create([
+            'room_id' => $request->room_id,
+            'customer' => $request->customer,
+            'guests' => $request->guests,
+            'start' => $request->start,
+            'end' => $request->end,
+        ]);
+
+        return response()->json(['message' => 'Booking created successfully.'], 201);
+    }
 }
+
