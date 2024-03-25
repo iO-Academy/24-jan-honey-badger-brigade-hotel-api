50cd27ad8dda1d615072005c778024cc8b1b17c1
Szabó Gergő <szabogergo20011008@gmail.com>
2024-03-25 14:36:18 +0100

    created Booking model, factory, migration and seeder

---
+++ database/seeders/BookingsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Database\Factories\BookingFactory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BookingsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+    }
+}
