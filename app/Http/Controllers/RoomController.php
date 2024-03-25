20adcdabda75cec20b4024d36cc7bb2172a47310
Szabó Gergő <szabogergo20011008@gmail.com>
2024-03-25 16:26:38 +0100

    name changes

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -21,7 +21,7 @@ class BookingController extends Controller
             'end' => 'required|date|after_or_equal:start',
         ]);
 
-        $isRoomAvailable = Booking::where('room_id', $request->room_id)
+        $isRoomAvailable = Booking::isAvailable('room_id', $request->room_id)
             ->where(function ($query) use ($request) {
                 $query->whereBetween('start', [$request->start, $request->end])
                     ->orWhereBetween('end', [$request->start, $request->end]);
@@ -32,7 +32,7 @@ class BookingController extends Controller
             return response()->json(['message' => 'The room is already booked for the given dates.'], 400);
         }
 
-        $room = Room::findOrFail($request->room_id);
+        $room = Room::checkCapacity($request->room_id);
         if ($request->guests > $room->max_capacity) {
             return response()->json(['message' => 'The number of guests exceeds the room\'s capacity.'], 400);
         }
