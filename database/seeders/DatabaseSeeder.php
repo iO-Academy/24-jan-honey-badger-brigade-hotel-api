4a08e3fbea1b274dfffb31f87a93d6c08d35d0f6
Ashton Jeffrey <ashtonjeffrey@live.com>
2024-03-25 11:57:29 +0000

    created room seeder and factory and ran seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            TypeSeeder::class,
+            RoomSeeder::class
         ]);
     }
 }
--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Room;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Psy\Util\Str;
 
 class RoomSeeder extends Seeder
 {
@@ -12,6 +16,11 @@ class RoomSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        for ($i = 0; $i < 10; $i++)
+        {
+            Room::factory()
+                ->recycle(Type::factory()->create())
+                ->count(3)->create();
+        }
     }
 }
