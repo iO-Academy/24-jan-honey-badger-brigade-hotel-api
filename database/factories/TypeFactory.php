db02b214e0636f282a8b3f5a0f51feabe7df21cc
Jenny <jenny@redsparkdigital.co.uk>
2024-03-25 11:18:16 +0000

    Type model and factory

--- database/factories/TypeFactory.php
+++ database/factories/TypeFactory.php
@@ -17,7 +17,7 @@ class TypeFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->sentence(3),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
         ]);
     }
 }
