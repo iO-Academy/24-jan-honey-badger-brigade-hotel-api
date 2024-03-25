1bb7f1101e135f2ffc067e89de6516312e4fb5e0
Emma Kim <eema.kim01@gmail.com>
2024-03-25 12:37:08 +0000

    room test for get all rooms

--- tests/Feature/RoomTest.php
+++ tests/Feature/RoomTest.php
@@ -3,14 +3,42 @@
 namespace Tests\Feature;
 
 use App\Models\Room;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class RoomTest extends TestCase
 {
+    use DatabaseMigrations;
     public function test_getRooms(): void
     {
+        Room::factory()->create();
+
+        $response = $this->getJson('/api/rooms');
+        $response->assertStatus(200)
+            ->assertJson(function (AssertableJson $json) {
+                $json->hasAll(['message', 'data'])
+                    ->whereType('message', 'string')
+                    ->has('data', 1, function (AssertableJson $json) {
+                        $json->hasAll(['id', 'name', 'image', 'min-capacity', 'max-capacity', 'type'])
+                            ->whereAllType([
+                                    'id' => 'integer',
+                                    'name' => 'string',
+                                    'image' => 'string',
+                                    'min-capacity' => 'integer',
+                                    'max-capacity' => 'integer',
+                                ])
+                            ->has('type', function (AssertableJson $json) {
+                                $json->hasAll(['id', 'name'])
+                                    ->whereAllType([
+                                        'id' => 'integer',
+                                        'name' => 'string'
+                                    ]);
+                            });
+                    });
+            });
 
     }
 }
