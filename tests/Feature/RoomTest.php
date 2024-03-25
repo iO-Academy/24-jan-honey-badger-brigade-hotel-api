4d9596ef1bebb9a899b7e122a53cedc03013fc0a
Emma Kim <eema.kim01@gmail.com>
2024-03-25 12:00:45 +0000

    adding test files

---
+++ tests/Feature/RoomTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Room;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RoomTest extends TestCase
+{
+    public function test_getRooms(): void
+    {
+
+    }
+}
