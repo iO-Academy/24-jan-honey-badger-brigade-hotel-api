ac116bd7ac01c01591ca535ae4dd83d1bf011ba7
Szabó Gergő <szabogergo20011008@gmail.com>
2024-03-25 14:36:18 +0100

    created Booking model, factory, migration and seeder

---
+++ database/factories/BookingFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Booking;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Booking>
+ */
+class BookingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Booking::class;
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
