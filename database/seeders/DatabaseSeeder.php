2dcce681e5f1027a403c5e2ea6423bc5905f38a7
Jenny <jenny@redsparkdigital.co.uk>
2024-03-25 11:35:04 +0000

    update database seeder call

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-
+            TypeSeeder::class,
+            RoomSeeder::class
         ]);
     }
 }
