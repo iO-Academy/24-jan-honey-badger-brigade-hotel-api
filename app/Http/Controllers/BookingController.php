62cfa51053d790a611d2f7d33b28dc27550be62e
Szabó Gergő <szabogergo20011008@gmail.com>
2024-03-25 13:03:58 +0100

    -m 'yet more merge conflicts resolved'added BookingController

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Models\Booking;
 use App\Models\Room;
-
 use App\Services\CheckAvailabilityService;
 use App\Services\JsonResponseService;
 use Illuminate\Http\JsonResponse;
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
+
     private JsonResponseService $responseService;
 
     private CheckAvailabilityService $availabilityService;
@@ -73,5 +74,15 @@ class BookingController extends Controller
             'Bookings successfully retrieved.',
             $bookings
         ));
+    }
+
+    public function index()
+    {
+        $bookings = Booking::where('end_date','›', now())
+                    -›orderBy('start_date', 'asc')
+                    -›with('customer','room')
+                    -›get(['id','customer','start_date', 'end_date', 'room_id']);
+
+        return response()-›json($bookings);
     }
 }
