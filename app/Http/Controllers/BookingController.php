5584eb5ee29d812380f6d17dfea3ec392bdf2731
Jenny <jenny@redsparkdigital.co.uk>
2024-03-26 15:22:51 +0000

    availability service only checks dates

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
+use App\Models\Room;
 use App\Services\CheckAvailabilityService;
 use App\Services\JsonResponseService;
 use Illuminate\Http\JsonResponse;
@@ -10,6 +12,7 @@ use Illuminate\Http\Request;
 class BookingController extends Controller
 {
     private JsonResponseService $responseService;
+
     private CheckAvailabilityService $availabilityService;
 
     public function __construct(JsonResponseService $responseService, CheckAvailabilityService $availabilityService)
@@ -26,8 +29,20 @@ class BookingController extends Controller
                 $result
             ), 400);
         }
+        $room = Room::find($request->room_id);
+        if ($request->guests < $room->min_capacity || $request->guests > $room->max_capacity) {
+            return response()->json($this->responseService->getFormat(
+                'The '.$room->name.' can only accommodate between '.$room->min_capacity.' and '.$room->max_capacity.' guests.'
+            ), 400);
+        }
+        $newBooking = new Booking();
+        $newBooking->room_id = $request->room_id;
+        $newBooking->customer = $request->customer;
+        $newBooking->guests = $request->guests;
+        $newBooking->start = $request->start;
+        $newBooking->end = $request->end;
 
-        if (! $result->save()) {
+        if (! $newBooking->save()) {
             return response()->json($this->responseService->getFormat(
                 'Failed to create booking'
             ), 500);
