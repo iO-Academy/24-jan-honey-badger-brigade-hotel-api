b57f753ee74e51a5583d5fbb89dca77a41254994
Ashton Jeffrey <ashtonjeffrey@live.com>
2024-03-26 09:23:34 +0000

    created HTTP test for returning all types API route

--- tests/Feature/TypeTest.php
+++ tests/Feature/TypeTest.php
@@ -2,17 +2,30 @@
 
 namespace Tests\Feature;
 
+use App\Models\Type;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class TypeTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function test_getAllTypes(): void
     {
-        $response = $this->get('/');
+        Type::factory()->create();
 
-        $response->assertStatus(200);
+        $response = $this->getJson('/api/types');
+
+        $response->assertOk()
+            ->assertJson(function (AssertableJson $json) {
+                $json->hasAll(['message', 'data'])
+                    ->whereType('message', 'string')
+                    ->has('data', 1, function (AssertableJson $json) {
+                        $json->hasAll(['id', 'name'])
+                            ->whereAllType([
+                                'id'=>'integer',
+                                'name'=>'string'
+                            ]);
+                    });
+            });
+
     }
 }
