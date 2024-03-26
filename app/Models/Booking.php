75f40a9684dd24e84c4ed8aa0daf18d37ff95b44
Szabó Gergő <szabogergo20011008@gmail.com>
2024-03-26 10:51:26 +0100

    booking model methods

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Booking extends Model
 {
@@ -13,4 +15,13 @@ class Booking extends Model
     {
     }
 
+    public function room(): BelongsTo
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
