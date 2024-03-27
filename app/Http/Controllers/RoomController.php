6bd3f176e98918314dc61cf3dbe0c84b821f3c08
Jenny <jenny@redsparkdigital.co.uk>
2024-03-27 12:06:51 +0000

    adding if to type filter

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -17,15 +17,21 @@ class RoomController extends Controller
 
     public function all(Request $request)
     {
-        $type = '%';
         if ($request->type) {
             $type = $request->type;
+
+            return response()->json($this->responseService->getFormat(
+                'Rooms successfully retrieved',
+                Room::with(['type:id,name'])
+                    ->where('type_id', 'LIKE', $type)
+                    ->get()
+                    ->makeHidden(['rate', 'description', 'type_id'])
+            ));
         }
 
         return response()->json($this->responseService->getFormat(
             'Rooms successfully retrieved',
             Room::with(['type:id,name'])
-                ->where('type_id', 'LIKE', $type)
                 ->get()
                 ->makeHidden(['rate', 'description', 'type_id'])
         ));
